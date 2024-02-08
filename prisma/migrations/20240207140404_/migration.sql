269177c43eed787ab3a529056fb3f433bd74c576
Matheus da Silva de Paula <matheus.silva@startyourapp.com.br>
2024-02-08 19:07:19 -0300

    feat: last class

---
+++ prisma/migrations/20240207140404_/migration.sql
@@ -0,0 +1,22 @@
+-- CreateTable
+CREATE TABLE "Vote" (
+    "id" SERIAL NOT NULL,
+    "sessionId" TEXT NOT NULL,
+    "pollId" TEXT NOT NULL,
+    "pollOptionId" TEXT NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+
+    CONSTRAINT "Vote_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "Vote_sessionId_key" ON "Vote"("sessionId");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "Vote_pollId_key" ON "Vote"("pollId");
+
+-- AddForeignKey
+ALTER TABLE "Vote" ADD CONSTRAINT "Vote_pollOptionId_fkey" FOREIGN KEY ("pollOptionId") REFERENCES "PollOption"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "Vote" ADD CONSTRAINT "Vote_pollId_fkey" FOREIGN KEY ("pollId") REFERENCES "poll"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
---
+++ src/http/routes/vote-on-poll.ts
@@ -0,0 +1,69 @@
+import { z } from "zod"
+import { prisma } from "../../lib/prisma"
+import { FastifyInstance } from "fastify"
+import { randomUUID } from "crypto"
+import { redis } from "../../lib/redis"
+
+export async function voteOnPoll(app: FastifyInstance) {
+    app.post('/polls/:pollId/votes', async (request, reply) => {
+        const voteBody = z.object({
+            pollOptionId: z.string().uuid()
+        })
+
+        const voteOnPollParams = z.object({
+            pollId: z.string().uuid()
+        })
+
+        const { pollOptionId } = voteBody.parse(request.body)
+
+        const { pollId } = voteOnPollParams.parse(request.params)
+
+        let sessionId = request.cookies.session
+
+        if (sessionId) {
+            const userPreviousVote = await prisma.vote.findUnique({
+                where: {
+                    sessionId: sessionId,
+                    pollId
+                }
+            })
+            if (userPreviousVote && userPreviousVote.pollOptionId !== pollOptionId) {
+                await prisma.vote.delete({
+                    where: {
+                        id: userPreviousVote.id
+                    }
+                })
+                await redis.zincrby(pollId, -1, userPreviousVote.pollOptionId)
+
+            } else if (userPreviousVote) {
+                return reply.status(400).send({ message: 'You already vote on this poll' })
+            }
+        }
+        if (!sessionId) {
+            sessionId = randomUUID()
+
+            reply.setCookie('session', sessionId, {
+                path: '/',
+                maxAge: 60 * 60 * 24 * 30,
+                signed: true,
+                httpOnly: true
+            })
+
+        }
+
+
+        await prisma.vote.create({
+            data: {
+                sessionId,
+                pollId,
+                pollOptionId
+
+            }
+        })
+
+        await redis.zincrby(pollId, 1, pollOptionId)
+
+        return reply.status(201).send()
+    })
+
+}
